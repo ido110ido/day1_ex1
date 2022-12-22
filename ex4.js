0d2829949d25c99a715aa28e635af664dbc2ae87
Ido Atias <ido110ido@gmail.com>
2022-12-22 09:40:17 +0200

    day6_dark_mood

--- ex4.js
+++ ex4.js
@@ -7,3 +7,30 @@
 function delayHandleClick(Id){
     setTimeout(() => handleClick(Id), 3000);
 }
+
+function Clicked(){
+        document.body.classList.toggle('dark_mood')
+        document.getElementById("head").classList.toggle('dark_mood_header')
+        document.getElementById("h1").classList.toggle('dark_mood_h1')
+    
+}
+
+function checkPasswordStrength(password) {
+    if (password.length < 8) {
+      return "weak password";
+    }
+    if (!password.match(/[a-z]/)) {
+      return "weak password";
+    }
+    if (!password.match(/[A-Z]/)) {
+      return "weak password";
+    }
+    if (!password.match(/[0-9]/)) {
+      return "weak password";
+    }
+    if (password.match(/\s/)) {
+      return "weak password";
+    }
+    return "strong password";
+  }
+  
